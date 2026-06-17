addc695f2b182a40d43609179bfd9755498ee0b0
jiwan <jiwan1230@naver.com>
2026-06-18 00:52:34 +0900

    modified test.py

--- gear_move/test.py
+++ gear_move/test.py
@@ -1,58 +1,58 @@
-start = posx(574.07, 149.69, 340.0, 0.0,-180.0, 0.0)
-set_digital_output(2,ON)
-set_digital_output(1,OFF)
+# start = posx(574.07, 149.69, 340.0, 0.0,-180.0, 0.0)
+# set_digital_output(2,ON)
+# set_digital_output(1,OFF)
 
-mwait(1)
+# mwait(1)
 
-# 초기 위치로 이동 및 gripper 설정
+# # 초기 위치로 이동 및 gripper 설정
 
-#a = get_current_posx()[0]
-a = start
+# #a = get_current_posx()[0]
+# a = start
 
-force_ext = get_tool_force(DR_BASE)
-tp_popup("{}".format(force_ext))
+# force_ext = get_tool_force(DR_BASE)
+# tp_popup("{}".format(force_ext))
 
-amovel(-100, 15, 15)
+# amovel(-100, 15, 15)
 
-while 1:
-    # if force_ext[2] <= 30:
-    #     # a[2] = a[2] - 1
-    #     # movel(a, 50, 50)
+# while 1:
+#     # if force_ext[2] <= 30:
+#     #     # a[2] = a[2] - 1
+#     #     # movel(a, 50, 50)
     
-    # else:
-    #     tp_popup("Ground Detected")   
-    #     a[2] = a[2] + 15
-    #     set_digital_output(2,OFF)
-    #     set_digital_output(1,ON)
-    #     mwait(1)
-    #     break
+#     # else:
+#     #     tp_popup("Ground Detected")   
+#     #     a[2] = a[2] + 15
+#     #     set_digital_output(2,OFF)
+#     #     set_digital_output(1,ON)
+#     #     mwait(1)
+#     #     break
 
-    if force_ext[2] > 30 :
-        stop(DR_SSTOP)
-        a[2] += 15
-        movel(a, 50, 50)
-        set_digital_output(2,OFF)
-        set_digital_output(1,ON)
-        mwait(1)
-        break
+#     if force_ext[2] > 30 :
+#         stop(DR_SSTOP)
+#         a[2] += 15
+#         movel(a, 50, 50)
+#         set_digital_output(2,OFF)
+#         set_digital_output(1,ON)
+#         mwait(1)
+#         break
 
 
-movel(start, 50, 50)
+# movel(start, 50, 50)
 
-s2 = posx(276.04, 149.69, 340.0, 0.0,-180.0, 0.0)
-movel(s2, 50, 50)
+# s2 = posx(276.04, 149.69, 340.0, 0.0,-180.0, 0.0)
+# movel(s2, 50, 50)
 
-a = get_current_posx()[0]
+# a = get_current_posx()[0]
 
-while 1:
-    if force_ext[2] <= 30:
-        a[2] = a[2] - 1
-        movel(a, 50, 50)
+# while 1:
+#     if force_ext[2] <= 30:
+#         a[2] = a[2] - 1
+#         movel(a, 50, 50)
 
-    else:
-        tp_popup("Ground Detected")   
-        a[2] = a[2] + 15
-        set_digital_output(2,ON)
-        set_digital_output(1,OFF)
-        mwait(1)
-        break
+#     else:
+#         tp_popup("Ground Detected")   
+#         a[2] = a[2] + 15
+#         set_digital_output(2,ON)
+#         set_digital_output(1,OFF)
+#         mwait(1)
+#         break
